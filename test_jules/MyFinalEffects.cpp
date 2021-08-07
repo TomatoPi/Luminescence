618ab626abe1e1f9476ff60c16f6b51db129b215
Jules Fouchy <jules.fouchy@ntymail.com>
2021-08-08 00:45:15 +0200

    Fix : forgot to clamp the palettes

--- test_jules/MyFinalEffects.cpp
+++ test_jules/MyFinalEffects.cpp
@@ -2,11 +2,19 @@
 #include "ColorPalette.h"
 #include "EffectsMixer.h"
 
+static vec3 clamp01(vec3 v)
+{
+    return vec3(
+        min(max(v.x, 0.f), 1.f),
+        min(max(v.y, 0.f), 1.f),
+        min(max(v.z, 0.f), 1.f));
+}
+
 namespace FinalEffects {
 
 FINAL_EFFECT(ScrollingGradient)
 {
-    return palette_rainbow.eval(in.pos + in.time);
+    return clamp01(palette_test6.eval(in.pos * 0.1 + in.time));
 }
 
 } // namespace FinalEffects
