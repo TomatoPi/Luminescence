fc85c8c07311caff18506dab242bd11d30bd2705
DAGO Kokri Esaïe <dago.esaie@protonmail.com>
2021-08-15 07:06:54 +0200

    Tests with half of total leds

--- driver/driver.ino
+++ driver/driver.ino
@@ -11,9 +11,10 @@ using Palette = color_pallette_t<color_t, index_t, coef_t>;
 
 #include "palettes.h"
 
-static constexpr index_t MaxLedsCount = 14;
+static constexpr index_t MaxLedsCount = 30 * 21;
 
-Range pouet = Range::map_on_pixel_index(0, 14, 14, 0);
+coef_t master_clock = coef_t(0);
+Range pouet = Range::map_on_pixel_index(0, MaxLedsCount, MaxLedsCount, 0);
 color_t leds[MaxLedsCount];
 
 void setup()
@@ -21,6 +22,7 @@ void setup()
   Serial.begin(9600);
   while (!Serial) ;
   FastLED.addLeds<NEOPIXEL, 2>(leds, MaxLedsCount);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 10000);
 
   memset(leds, 32, sizeof(CRGB) * MaxLedsCount);
   FastLED.show();
@@ -28,7 +30,24 @@ void setup()
 
 void loop()
 {
-  palette_rainbow.eval_range(leds, pouet);
+// Too slow...
+//  master_clock = fmod(master_clock + coef_t(0.01f), coef_t(1.f));
+//  pouet = Range::map_on_pixel_index(0, MaxLedsCount, MaxLedsCount, master_clock);
+//  palette_rainbow.eval_range(leds, pouet);
+
+// Cheaper prototype. Use integer arithmetics
+  static unsigned long master_clock = 0;
+  static unsigned long master_clock_period = 2000;
+  master_clock = millis();
+  uint8_t value = ((master_clock % master_clock_period) * 255) / master_clock_period;
+  for (unsigned int i = 0 ; i < MaxLedsCount ; ++i)
+  {
+    leds[i] = CRGB(
+      value,
+      value + 64, // No need for modulo thanks to overflow
+      value + i   // Create nice tracers
+    );
+  }
   FastLED.show();
   delay(10);
 }
--- driver/palettes.h
+++ driver/palettes.h
@@ -1,66 +1,66 @@
 #pragma once
 
-static const color_pallette_t<CRGB, uint32_t, float> palette_rainbow = {
+static const Palette palette_rainbow = {
     {0.5f, 0.5f, 0.5f},
     {0.5f, 0.5f, 0.5f},
     {1.f, 1.f, 1.f},
     {0.f, 0.33f, 0.67f}};
 
-static const color_pallette_t<CRGB, uint32_t, float> palette_deep = {
+static const Palette palette_deep = {
     {0.5f, 0.5f, 0.5f},
     {0.5f, 0.5f, 0.5f},
     {2.f, 1.f, 0.f},
     {0.5f, 0.2f, 0.25f}};
 
-static const color_pallette_t<CRGB, uint32_t, float> palette_coper = {
+static const Palette palette_coper = {
     {0.8f, 0.5f, 0.4f},
     {0.2f, 0.4f, 0.2f},
     {2.f, 1.f, 1.f},
     {0.f, 0.25f, 0.25f}};
 
-static const color_pallette_t<CRGB, uint32_t, float> palette_yellow_blue = {
+static const Palette palette_yellow_blue = {
     {0.5f, 0.5f, 0.5f},
     {0.5f, 0.5f, 0.5f},
     {1.f, 1.f, 0.5f},
     {0.8f, 0.9f, 0.3f}};
 
-static const color_pallette_t<CRGB, uint32_t, float> palette_red_blue = {
+static const Palette palette_red_blue = {
     {2.000000f, 0.170000f, 1.890000f},
     {2.000000f, 0.476000f, 1.515000f},
     {1.000000f, 1.000000f, 2.000000f},
     {0.957000f, 0.857000f, 0.935000f}};
 
-static const color_pallette_t<CRGB, uint32_t, float> palette_test = {
+static const Palette palette_test = {
     {0.000000f, 1.318000f, 0.035000f},
     {0.952000f, 1.734000f, 1.740000f},
     {1.000000f, 1.000000f, 2.000000f},
     {1.343000f, 1.594000f, 0.935000f}};
 
-static const color_pallette_t<CRGB, uint32_t, float> palette_test2 = {
+static const Palette palette_test2 = {
     {0.500000f, 0.500000f, 0.500000f},
     {0.500000f, 0.500000f, 0.500000f},
     {1.000000f, 1.000000f, 1.000000f},
     {0.000000f, 0.330000f, 1.410000f}};
 
-static const color_pallette_t<CRGB, uint32_t, float> palette_test3 = {
+static const Palette palette_test3 = {
     {0.500000f, 0.500000f, 0.500000f},
     {0.546000f, 0.301000f, 0.155000f},
     {1.000000f, 1.000000f, 1.000000f},
     {0.000000f, 1.063000f, 1.410000f}};
 
-static const color_pallette_t<CRGB, uint32_t, float> palette_test4 = {
+static const Palette palette_test4 = {
     {0.500000f, 0.500000f, 0.500000f},
     {0.546000f, 0.301000f, 0.155000f},
     {1.000000f, 1.000000f, 1.000000f},
     {0.000000f, 0.642000f, 1.410000f}};
 
-static const color_pallette_t<CRGB, uint32_t, float> palette_test5 = {
+static const Palette palette_test5 = {
     {1.794000f, 0.125000f, 0.000000f},
     {1.820000f, 0.216000f, 0.835000f},
     {1.000000f, 1.000000f, 1.000000f},
     {0.952000f, 0.642000f, 1.410000f}};
 
-static const color_pallette_t<CRGB, uint32_t, float> palette_test6 = {
+static const Palette palette_test6 = {
     {1.794000f, 0.125000f, 2.000000f},
     {1.820000f, 0.216000f, 2.000000f},
     {1.000000f, 1.000000f, 1.000000f},
