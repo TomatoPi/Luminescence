ec729016731e3bb9576fa066b0e1178607c53233
Jules Fouchy <jules.fouchy@ntymail.com>
2021-08-21 00:52:13 +0200

    [palettes] add some + link to a tool

--- driver/palettes.h
+++ driver/palettes.h
@@ -1,34 +1,48 @@
 #pragma once
 
+/**
+ * 
+ * Palettes can be easily generated using this tool :
+ * https://github.com/JulesFouchy/ColorPaletteGenerator
+ * 
+ */
+
 namespace Palettes {
 
   static constexpr ColorPalette rainbow {
-  //  min  max  frequency*60  phase
-    { 0,   255,    60,      0  }, // RED
-    { 0,   255,    60,     85  }, // GREEN
-    { 0,   255,    60,    171  }  // BLUE
+  // min  max  f*60 phase
+    {  0, 255,  60,   0}, // RED
+    {  0, 255,  60,  85}, // GREEN
+    {  0, 255,  60, 171}, // BLUE
   };
 
   static constexpr ColorPalette deep_blue_and_bright_yellow {
-  //  min  max  frequency*60  phase
-    { 0,   255,    60,      0  }, // RED
-    { 0,   255,    60,     25  }, // GREEN
-    { 0,   255,    60,     51  }  // BLUE
+  // min  max  f*60 phase
+    {  0, 255,  60,  25}, // GREEN
+    {  0, 255,  60,   0}, // RED
+    {  0, 255,  60,  51}, // BLUE
   };
 
   // Realy Nice :)
-  static constexpr ColorPalette test {
-  //  min  max  frequency*60  phase
-    { 0,   255,    60,        127  }, // RED
-    { 0,   255,    60,         51  }, // GREEN
-    { 0,   255,    0,          64  }  // BLUE
+  static constexpr ColorPalette pastel {
+  // min  max  f*60 phase
+    {  0, 255,  60, 127}, // RED
+    {  0, 255,  60,  51}, // GREEN
+    {  0, 255,   0,  64}, // BLUE
   };
 
   static constexpr ColorPalette warm_beech {
-  //  min  max  frequency*60  phase
-    { 0,   127,      30,      255 }, // RED
-    { 25,  127,      30,      255 }, // GREEN
-    { 51,  127,      30,      255 }  // BLUE
+  // min  max  f*60 phase
+    {  0, 127,  30, 255 }, // RED
+    { 25, 127,  30, 255 }, // GREEN
+    { 51, 127,  30, 255 }, // BLUE
+  };
+
+  static constexpr ColorPalette synthwave {
+  // min  max  f*60 phase
+    { 69, 255, 120, 120}, // RED
+    {  0,  56,   0,  84}, // GREEN
+    {111, 255,   0,  39}, // BLUE
   };
 
   const ColorPalette& Get(uint8_t idx)
@@ -37,9 +51,9 @@ namespace Palettes {
     {
       case 0: return rainbow;
       case 1: return deep_blue_and_bright_yellow;
-      case 2: return test;
-      case 3:
-      case 4:
+      case 2: return pastel;
+      case 3: return warm_beech;
+      case 4: return synthwave;
       case 5:
       case 6:
       case 7:
