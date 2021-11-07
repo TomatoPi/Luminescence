78840470b6c167eef991a6f2c95f4246a24c8c8e
DAGO Kokri Esaïe <dago.esaie@protonmail.com>
2021-11-07 15:03:44 +0100

    working raw bindings mecanism

--- jackproxy/apc40-mapping.cpp
+++ jackproxy/apc40-mapping.cpp
@@ -60,19 +60,49 @@
 #include <string.h>
 
 #include <string>
+#include <vector>
+#include <functional>
+#include <unordered_map>
 
 const char* path_from_apc = NULL;
 const char* path_to_apc = NULL;
 
-
 volatile int is_running = 1;
+
+struct binding_t
+{
+  uint8_t        midikey[2];
+  const char*   command;
+  std::function<std::string(uint8_t)> midival_to_str;
+  std::function<uint8_t(const char*)> str_to_midival;
+
+  uint16_t key() const { return ((uint16_t)midikey[0]) << 8 | midikey[1]; }
+};
+
+std::string uint8_to_str(uint8_t val) { return std::to_string(val); }
+uint8_t str_to_uint8(const char* str) { int res; sscanf(str, "%d", &res); return res; }
+
+std::vector<binding_t>                                  bindings_list;
+std::unordered_multimap<int16_t, const binding_t*>      midi_to_command_map;
+std::unordered_multimap<std::string, const binding_t*>  command_to_midi_map;
 
 void register_mappings()
 {
+  // Generate bindings
+  bindings_list = {
+    { {0xb0, 0x07}, "intensity:0", uint8_to_str, str_to_uint8 },
+    { {0x90, 0x35}, "colormod:0", uint8_to_str, str_to_uint8 },
+  };
 
+  // Generate tables
+  for (const auto& binding : bindings_list)
+  {
+    midi_to_command_map.emplace(binding.key(), &binding);
+    command_to_midi_map.emplace(binding.command, &binding);
+  }
 }
 
-std::string midistr_to_command(const char* midistr)
+std::vector<std::string> midistr_to_command(const char* midistr)
 {
 	uint8_t msg[512];
   ssize_t len = 0, nread = strlen(midistr), index = 0;
@@ -96,39 +126,63 @@ std::string midistr_to_command(const char* midistr)
   if (len != 3)
   {
     fprintf(stderr, "Unsupported midimsg : %s\n", midistr);
-    return std::string();
+    return {};
   }
 
-  uint16_t key = ((uint16_t)msg[0]) << 8 | msg[1];
-  uint8_t value = msg[2];
+  const uint16_t key = ((uint16_t)msg[0]) << 8 | msg[1];
   // search for key in mappings
-  if (1) // key not found
+  auto [begin, end] = midi_to_command_map.equal_range(key);
+  if (begin == end) // key not found
   {
-    fprintf(stderr, "Unbounded midi msg %s\n", midistr);
-    return std::string();
+    fprintf(stderr, "Unbounded midi msg %04x\n", key);
+    return {};
   }
 
-  // generate message from key and value
-  char command[512];
-  sprintf(command, "%04u %u", key, value);
-  return std::string(command);
+  const uint8_t value = msg[2];
+  std::vector<std::string> result;
+  for (auto itr = begin; itr != end; ++itr)
+  {
+    auto& [_, binding] = *itr;
+    char tmp[512];
+    sprintf(tmp, "%s %s", binding->command, binding->midival_to_str(value).c_str());
+    result.emplace_back(tmp);
+  }
+
+  return result;
 }
 
 // Search for mapping object in maping table
 // convert command args to raw midi str
 // return message length or -1 on error
-int command_to_midistr(const char* command, char* midistr)
+std::vector<std::string> command_to_midistr(const char* commandstr)
 {
-  if (0) // if command not found
+  char cmd[64], arg[64];
+  if (sscanf(commandstr, "%s %s", cmd, arg) != 2)
   {
-    fprintf(stderr, "Unbound command : %s\n", command);
-    return 0;
+    fprintf(stderr, "Invalid command : %s\n", commandstr);
+    return {};
   }
 
-  uint16_t key = 0; // get key from command
-  uint8_t value = 0;// convert args to value
-  sprintf(midistr, "%02x %02x %02x", key >> 8, key & 0xFF, value);
-  return 3;
+  auto [begin, end] = command_to_midi_map.equal_range(cmd);
+  if (begin == end) // key not found
+  {
+    fprintf(stderr, "Unbound command : %s\n", cmd);
+    return {};
+  }
+
+  std::vector<std::string> result;
+  for (auto itr = begin; itr != end; ++itr)
+  {
+    auto& [_, binding] = *itr;
+    uint16_t key = binding->key(); // get key from command
+    uint8_t value = binding->str_to_midival(arg);// convert args to value
+
+    char tmp[512];
+    sprintf(tmp, "%02x %02x %02x", key >> 8, key & 0xFF, value);
+    result.emplace_back(tmp);
+  }
+  
+  return result;
 }
 
 void sighandler(int sig)
@@ -176,34 +230,31 @@ int main(int argc, char* const argv[])
     }
     while (is_running && fgets(buffer, 512, apc40))
     {
-      std::string command = midistr_to_command(buffer);
-      if (command.length())
+      auto result = midistr_to_command(buffer);
+      for (auto& cmd : result)
       {
-        fprintf(stdout, "%s\n", command.c_str());
+        fprintf(stdout, "%s\n", cmd.c_str());
       }
     }
   }
   else // 0 != cpid : Parent : stdin -> toAPC
   {
-    char command_buffer[512], midibuffer[512];
+    char buffer[512];
     FILE* apc40 = fopen(path_to_apc, "w");
     if (!apc40)
     {
       perror("fopen to apc");
       exit(EXIT_FAILURE);
     }
-    while (is_running && fgets(command_buffer, 512, stdin))
+    while (is_running && fgets(buffer, 512, stdin))
     {
-      int msglen = command_to_midistr(command_buffer, midibuffer);
-      if (-1 == msglen)
+      auto result = command_to_midistr(buffer);
+      for (auto& msg : result)
       {
-        fprintf(stderr, "Error parsing command : %s", command_buffer);
-        exit(EXIT_FAILURE);
+        fprintf(apc40, "%s\n", msg.c_str());
+        fprintf(stderr, "To apc : %s\n", msg.c_str());
       }
-      else if (0 < msglen)
-      {
-        fprintf(apc40, "%s\n", midibuffer);
-      }
+      fflush(apc40);
     }
     kill(cpid, SIGTERM);
   }
