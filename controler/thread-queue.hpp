ac122231ee8e8000d115bbc4742ff2f836bb6d17
DAGO Kokri Esaïe <dago.esaie@protonmail.com>
2021-11-12 16:23:58 +0100

    removed debug prints

--- controler/thread-queue.hpp
+++ controler/thread-queue.hpp
@@ -18,7 +18,6 @@ public:
     void push(T&& item) {
       std::lock_guard lock(mutex);
       queue.push(std::forward<T>(item));
-      fprintf(stderr, "Pushed one object %lu\n", queue.size());
     }
 
     std::optional<T> pop() {
@@ -27,7 +26,6 @@ public:
         return std::nullopt;
       auto obj = std::make_optional<T>(queue.front());
       queue.pop();
-      fprintf(stderr, "Poped one object %lu\n", queue.size());
       return obj;
     }
 };
