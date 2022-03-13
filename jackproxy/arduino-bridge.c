b32b63b5b14614bd1bd17cc43007a0c72a78176c
Tomato <TomatoPi@coucou.fr>
2022-03-13 23:29:53 +0000

    update

--- jackproxy/arduino-bridge.c
+++ jackproxy/arduino-bridge.c
@@ -8,6 +8,17 @@
 #include <stdlib.h>
 #include <signal.h>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
 volatile int is_running = 1;
 
 void sighandler(int sig)
@@ -23,13 +34,67 @@ void sighandler(int sig)
 
 int main(int argc, char* const argv[])
 {
-  if (argc != 2)
+	struct addrinfo hints;
+	struct addrinfo *result, *rp;
+	int sfd, s;
+	struct sockaddr_storage peer_addr;
+	socklen_t peer_addr_len;
+	ssize_t nread;
+
+  if (argc != 3)
   {
-    fprintf(stderr, "Usage : %s <serial-port>\n", argv[0]);
+    fprintf(stderr, "Usage : %s <port> <serial-port>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
-  int serialfd = serialport_init(argv[1], B115200);
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+	hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+	hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+	hints.ai_protocol = 0;          /* Any protocol */
+	hints.ai_canonname = NULL;
+	hints.ai_addr = NULL;
+	hints.ai_next = NULL;
+
+	s = getaddrinfo(NULL, argv[1], &hints, &result);
+	if (s != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+		exit(EXIT_FAILURE);
+	}
+	
+	/* getaddrinfo() returns a list of address structures.
+	Try each address until we successfully bind(2).
+	If socket(2) (or bind(2)) fails, we (close the socket
+	and) try the next address. */
+
+	for (rp = result; rp != NULL; rp = rp->ai_next)
+	{
+		sfd = socket(rp->ai_family, rp->ai_socktype,
+		       rp->ai_protocol);
+		if (sfd == -1)
+		   continue;
+
+		if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0)
+		   break; /* Success */
+
+		close(sfd);
+	}
+
+	if (rp == NULL)
+	{	/* No address succeeded */
+		fprintf(stderr, "Could not bind\n");
+		exit(EXIT_FAILURE);
+	}
+
+	freeaddrinfo(result); /* No longer needed */
+
+	if (-1 == listen(sfd, 10))
+	{
+		perror("Failed to listen on port");
+		exit(EXIT_FAILURE);
+	}
+
+  int serialfd = serialport_init(argv[2], B115200);
   if (serialfd < 0)
   {
     fprintf(stderr, "Can't open serial port\n");
@@ -48,7 +113,7 @@ int main(int argc, char* const argv[])
     exit(EXIT_FAILURE);
   }
 
-  char* buffer[512];
+  char* buffer[4096];
 
   if (0 == cpid) // Child : Arduino -> stdout
   {
@@ -78,25 +143,33 @@ int main(int argc, char* const argv[])
     }
   }
   else // 0 != cpid : Parent : stdin -> arduino
-  {
+  while (is_running) {
+    peer_addr_len = sizeof(struct sockaddr_storage);
+    int client_fd = accept(sfd, (struct sockaddr *) &peer_addr, &peer_addr_len);
     while (is_running)
     {
-      ssize_t nread = fread(buffer, 1, 1, stdin);
-      if (-1 == nread)
+      nread = read(client_fd, buffer, 4096);
+      if (nread == -1)
       {
-        perror("read from stdin");
-        exit(EXIT_FAILURE);
+        perror("Read failed");
+        continue;               /* Ignore failed request */
       }
-      if (write(serialfd, buffer, nread) != nread)
+
+      if (s == 0)
       {
-        perror("write to arduino");
-        exit(EXIT_FAILURE);
+        if (write(serialfd, buffer, nread) != nread)
+        {
+          perror("write to arduino");
+          exit(EXIT_FAILURE);
+        }
+        serialport_flush(serialfd);
+        fprintf(stderr, "Wrote %zd bytes to arduino\n", nread);
+        usleep(100);
       }
-      serialport_flush(serialfd);
-      fprintf(stderr, "Wrote %zd bytes to arduino\n", nread);
-      usleep(100);
+      else
+        fprintf(stderr, "getnameinfo: %s\n", gai_strerror(s));
     }
-    kill(cpid, SIGTERM);
+//    kill(cpid, SIGTERM);
   }
 
   return 0;
